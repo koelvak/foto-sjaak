7b0c12936216b95fd6c475d0a612ee0f6dd34d0e
martijn geurs <marijn.geurs@gmail.com>
2013-11-28 16:01:01 +0100

    logboek ingevuld

---
+++ fotosjaak/config/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('SERVERNAME', 'localhos');
+	define('USERNAME', 'root');
+	define('PASSWORD', '');
+	define('DATABASENAME', 'am1b-fotosjaak');
+	date_default_timezone_set("Europe/Amsterdam");
+?>

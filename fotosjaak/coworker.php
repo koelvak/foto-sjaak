93c8eee11370c2252d7200279dcf46dd3277adea
martijn geurs <marijn.geurs@gmail.com>
2014-01-31 11:48:29 +0100

    niwuew pagina toegevoegd met een plaatje die je doorstuurd naar wikipedia

---
+++ fotosjaak/coworker.php
@@ -0,0 +1,15 @@
+<?php
+        $userrole = array('coworker', 'root', 'admin');
+        include('security.php');
+?>
+<table class='simple'>
+        <caption>crocodile</caption>
+  
+        <tr>
+                <td>
+                	<a href= 'http://nl.wikipedia.org/wiki/Fiat_Panda'>
+                        <img src='./images/Crocodile-panda.png' width=487 hight=362/>
+                        
+                </td>
+  </table>
+  
